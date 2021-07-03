07da63fe68b908cbb7f4401eca904efafd82dafe
Mehedi0911 <mmraaj378@gmail.com>
2021-07-03 11:27:02 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//data types
+// string - "its working fine" / "0171298734234"
+// number - 10
+// boolean - true/false
+
+//variable - var | let | const
+
+var x = 10;
+var y = 15;
+var z ="riyadh"
+console.log(x + y + z);
+
+var isHot = true;
+
+//loops
+//functions
+//arrays
+//methods
+
+
+
